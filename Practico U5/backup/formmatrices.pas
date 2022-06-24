46d5f0c8b00c3b5a4bc918354b857f8f5ca466b2
Alejandro Mogro <85258122+Jalx1000@users.noreply.github.com>
2022-06-24 16:27:06 -0400

    Add files via upload

---
+++ Practico U5/backup/formmatrices.pas
@@ -0,0 +1,109 @@
+unit FormMatrices;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Grids, StdCtrls,
+  UMatrices;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    StringGrid1: TStringGrid;
+    StringGrid2: TStringGrid;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+    procedure descargar;
+    procedure descargar2;
+  public
+	  m:Matriz;
+    m2:Matriz;
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  m:=Matriz.crear();
+  m.cargar();
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  descargar();
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+  m2:=Matriz.crear();
+  m2.cargar();
+end;
+
+procedure TForm1.Button4Click(Sender: TObject);
+begin
+  descargar2();
+end;
+
+procedure TForm1.Button5Click(Sender: TObject);
+begin
+  m.suma(m2);
+  descargar();
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.descargar;
+var i,j,f,c:integer;
+begin
+  f:=m.getFilas();
+  c:=m.getCols();
+  StringGrid1.RowCount:=f;
+  StringGrid1.ColCount:=c;
+  for i:=1 to f do begin
+    for j:=1 to c do begin
+      StringGrid1.Cells[j-1,i-1]:=IntToStr(m.getElem(i,j));
+    end;
+  end;
+end;
+
+procedure TForm1.descargar2;
+var i,j,f,c:integer;
+begin
+  f:=m2.getFilas();
+  c:=m2.getCols();
+  StringGrid2.RowCount:=f;
+  StringGrid2.ColCount:=c;
+  for i:=1 to f do begin
+    for j:=1 to c do begin
+      StringGrid2.Cells[j-1,i-1]:=IntToStr(m2.getElem(i,j));
+    end;
+  end;
+end;
+
+end.
+
---
+++ Practico U5/umatrices.pas
@@ -0,0 +1,122 @@
+unit UMatrices;
+
+{$mode ObjFPC}{$H+}
+
+interface
+uses
+  Classes, SysUtils, Dialogs;
+const MAX_F=1024;
+const MAX_C=1024;
+type
+
+    { Matriz }
+
+    Matriz=class
+		private
+      f:integer;
+      c:integer;
+
+      elem:array [1..MAX_F,1..MAX_C] of integer;
+    public
+      constructor crear();
+      function getElem(fil,col:integer):integer;
+      function getFilas():integer;
+      function getCols():integer;
+      procedure cargar();
+      procedure setElem(fil,col,elemN:integer);
+      procedure setFilas(fil:integer);
+      procedure setColumnas(cols:integer);
+
+      procedure suma(otraMatriz:matriz);
+      procedure multiplicacion(otraMatriz:matriz);
+    end;
+implementation
+
+{ Matriz }
+
+constructor Matriz.crear;
+var i,j:integer;
+begin
+  f:=0;
+  c:=0;
+  for i:=1 to MAX_F do begin
+    for j:=1 to MAX_C do begin
+      elem[i,j]:=0;
+    end;
+  end;
+end;
+
+function Matriz.getElem(fil, col: integer): integer;
+//var i,j:integer;
+begin
+  Result:=elem[fil,col];
+end;
+
+function Matriz.getFilas: integer;
+begin
+  Result:=f;
+end;
+
+function Matriz.getCols: integer;
+begin
+  Result:=c;
+end;
+
+procedure Matriz.cargar;
+var i,j:integer;
+begin
+  f:=StrToInt(InputBox('Ingrese filas','',''));
+  c:=StrToInt(InputBox('Ingrese columnas','',''));
+  for i:=1 to f do begin
+    for j:=1 to c do begin
+      elem[i,j]:=StrToInt(InputBox('Introduzca Elem ','Fila: '+IntToStr(i)+' columna: '+IntToStr(j),''));
+    end;
+  end;
+end;
+
+procedure Matriz.setElem(fil, col, elemN: integer);
+//var i,j:integer;
+begin
+   elem[fil,col]:=elemN;
+end;
+
+procedure Matriz.setFilas(fil: integer);
+begin
+  f:=fil;
+end;
+
+procedure Matriz.setColumnas(cols: integer);
+begin
+  c:=cols;
+end;
+
+procedure Matriz.suma(otraMatriz: matriz);
+var fila,columna:integer;
+begin
+
+ if (self.getFilas()+Self.getCols())=(OtraMatriz.getFilas()+OtraMatriz.getCols()) then begin
+   for columna:=1 to c do begin
+ for fila:=1 to f do begin
+    elem[fila,columna]:=elem[fila,columna]+OtraMatriz.getElem(fila,columna);
+  end;
+ end;
+ end;
+
+end;
+
+procedure Matriz.multiplicacion(otraMatriz: matriz);
+var fila,columna:integer;
+begin
+
+ if (self.getFilas()+Self.getCols())=(OtraMatriz.getFilas()+OtraMatriz.getCols()) then begin
+   for columna:=1 to c do begin
+ for fila:=1 to f do begin
+    elem[fila,columna]:=elem[fila,columna]*OtraMatriz.getElem(fila,columna);
+  end;
+ end;
+ end;
+
+end;
+
+end.
+
